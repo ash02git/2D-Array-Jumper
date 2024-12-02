e370f715f55831ce490b502ae120c31d1c44a7a2
ash02git <ashwinabraham2001@gmail.com>
2024-12-02 19:21:13 +0530

    Created data for level

--- Array-Jumper/header/Level/LevelModel.h
+++ Array-Jumper/header/Level/LevelModel.h
@@ -1,8 +1,24 @@
 #pragma once
+#include "../../header/Level/LevelData.h"
 namespace Level
 {
 	class LevelModel
 	{
+		struct BoxDimensions
+		{
+			float box_width;
+			float box_height;
+			float box_spacing;
+			float box_spacing_percentage=0.3f;
+			float bottom_offset=200.0f;
+		};
 
+	private:
+		LevelData current_level_data;
+
+	public:
+		LevelModel();
+		~LevelModel();
+		BlockType getCurrentBoxValue(int currentPosition);
 	};
 }
